760a814f49dd09eaef9ce55090d5e0ec253fd657
postolenko <denis.postolenko@yandex.ua>
2016-08-20 16:21:14 +0300

    Algoritm

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/algoritm/index.html
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+    bodyHeight = w.innerHeight || e.clientHeight || g.clientHeight;
+
+
+});
